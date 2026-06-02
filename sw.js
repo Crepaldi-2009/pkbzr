914fd1035c54c479135d5bb6497e9e7a474221ea
Crepaldi-2009 <vini.crep9@gmail.com>
2026-06-02 19:50:42 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,43 @@
+const CACHE_NAME = "pokerpro-v1";
 
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./game.js",
+    "./ai.js",
+    "./poker-engine.js",
+    "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+        .then(cache => {
+
+            return cache.addAll(
+                FILES_TO_CACHE
+            );
+
+        })
+
+    );
+
+});
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+        .then(response => {
+
+            return response || fetch(event.request);
+
+        })
+
+    );
+
+});
